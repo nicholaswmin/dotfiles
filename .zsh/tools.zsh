3bed5c027d9f84303c61f2bc88500bb1cd4fdc56
Nicholas Kyriakides <nik.kyriakides@gmail.com>
2026-06-21 21:56:59 +0300

    fix: tools exit status

--- .zsh/tools.zsh
+++ .zsh/tools.zsh
@@ -2,11 +2,17 @@
 # development tool integrations
 
 # node version manager
-command -v fnm >/dev/null 2>&1 && eval "$(fnm env --use-on-cd)"
+if command -v fnm >/dev/null 2>&1; then
+  eval "$(fnm env --use-on-cd)"
+fi
 
 # kubectl completion
-command -v kubectl >/dev/null 2>&1 && source <(kubectl completion zsh)
+if command -v kubectl >/dev/null 2>&1; then
+  source <(kubectl completion zsh)
+fi
 
 # heroku autocomplete
 HEROKU_AC_ZSH_SETUP_PATH="$HOME/Library/Caches/heroku/autocomplete/zsh_setup"
-[[ -f "$HEROKU_AC_ZSH_SETUP_PATH" ]] && source "$HEROKU_AC_ZSH_SETUP_PATH"
+if [[ -f "$HEROKU_AC_ZSH_SETUP_PATH" ]]; then
+  source "$HEROKU_AC_ZSH_SETUP_PATH"
+fi
