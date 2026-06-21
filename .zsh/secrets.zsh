f6bba1ae2db13a571d53ac6554e9c19f696f0b67
Nicholas Kyriakides <nik.kyriakides@gmail.com>
2026-06-22 00:56:37 +0300

    fix: secret keychain path

--- .zsh/secrets.zsh
+++ .zsh/secrets.zsh
@@ -1,14 +1,14 @@
 # load_secret VAR service - export VAR from the `environment` keychain; fail loud
 load_secret() {
   local v
-  v=$(security find-generic-password -s "$2" -w environment) || {
+  v=$(security find-generic-password -s "$2" -w "$HOME/Library/Keychains/environment.keychain-db") || {
     print -u2 "load_secret: '$2' not in the environment keychain"; return 1
   }
   export "$1=$v"
 }
 # secret service - print a secret on demand; nothing lingers in the environment
 secret() {
-  security find-generic-password -s "$1" -w environment || {
+  security find-generic-password -s "$1" -w "$HOME/Library/Keychains/environment.keychain-db" || {
     print -u2 "secret: '$1' not in the environment keychain"; return 1
   }
 }
